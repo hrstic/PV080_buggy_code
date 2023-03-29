0f74206b9a16ebd53dd000dfa639f0ff5af05691
hrstic <129248884+hrstic@users.noreply.github.com>
2023-03-29 08:56:58 +0200

    trailing whitespace and missing docstring fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import os
 import yaml
 import flask
@@ -24,6 +24,7 @@ def print_nametag(format_string, person):
 
 
 def fetch_website(urllib_version, url):
+    """A dummy docstring."""
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
@@ -52,8 +53,8 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
